555d62caddb7974366a1c8acfab09b268f33374b
Lucas Jorge <lucas1jorge@gmail.com>
2019-02-17 15:51:49 -0300

    Update img_raster_to_svg.py

--- img_raster_to_svg/img_raster_to_svg.py
+++ img_raster_to_svg/img_raster_to_svg.py
@@ -4,7 +4,7 @@
 
 # Replace "raster.png" on line 15 with the name of your raster image
 
-# Set the scale in line 25. Smaller scale means smaller radius of the vector spheres (replacing pixels), and thus leads to better definition:
+# Set the scale on line 25. Smaller scale means smaller radius of the vector spheres (replacing pixels), and thus leads to better definition:
 # ↓ scale - definition ↑
 
 
